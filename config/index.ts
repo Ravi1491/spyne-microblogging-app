20b25c1cbfa09132a1c4dd809487dcc1f7eb5b24
Ravi Singh <ravi149185@gmail.com>
2024-06-12 00:49:45 +0530

    Add database configuration

--- config/index.ts
+++ config/index.ts
@@ -5,4 +5,14 @@ export const applicationConfig = {
     env: process.env.NODE_ENV,
     port: process.env.PORT,
   },
+
+  // Database
+  db: {
+    dbDialect: process.env.DB_DIALECT || 'postgres',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { Dialect } from 'sequelize';
+import { applicationConfig } from 'config';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_DIALECT: Joi.string(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.number().default(5432),
+        DB_USER: Joi.string(),
+        DB_PASSWORD: Joi.string().allow(''),
+        DB_NAME: Joi.string(),
+        PORT: Joi.number().default(8080),
+        ENV: Joi.string()
+          .valid('development', 'base', 'beta', 'qa', 'qa2')
+          .default('development'),
+      }),
+    }),
+    SequelizeModule.forRoot({
+      dialect: applicationConfig.db.dbDialect as Dialect,
+      host: applicationConfig.db.host,
+      username: applicationConfig.db.user,
+      password: applicationConfig.db.password,
+      port: parseInt(applicationConfig.db.port, 10),
+      database: applicationConfig.db.name,
+      logging: false,
+      autoLoadModels: true,
+      // synchronize: true,
+      // sync: { alter: true },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
